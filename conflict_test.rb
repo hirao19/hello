b2e4ef0e59927f73180cfbafbcbe23c22ec6a829
hirao <shiki.19@icloud.com>
2019-12-23 19:40:32 +0900

    add_each_def

---
+++ conflict_test.rb
@@ -0,0 +1,19 @@
+(1..10).each do |num|
+  puts "#{num}回目のチャレンジ"
+end
+
+def name(bill)
+  puts bill + "様"
+  puts "今月の請求書でございます"
+  puts "また来月も宜しくお願い致します"
+end
+name("yamada")
+
+def charge(bill)
+  if bill < 0
+    return "表示できません"
+  end
+  return (bill * 1.1).to_i
+end
+puts charge(4000)
+puts charge(-10)
